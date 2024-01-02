544523d3943c3ca429230d924b3294d72355719b
Cheolmin99 <cjfals8570@naver.com>
2024-01-02 19:49:29 +0900

    투두 프로젝트 기능 완

---
+++ todo/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
+
+export default function TodoList({ filter }) {
+    const [todos, setTodos] = useState([
+        {id: '1', text: '공부하기', status: 'active'},
+        {id: '2', text: '운동하기', status: 'active'}
+    ]);
+    const handleAdd = (todo) => setTodos([...todos, todo]);
+    const handleUpdate = (updated) => setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+    const handleDelete = (deleted) => setTodos(todos.filter((todo) => (todo.id !== deleted.id)));
+    const filtered = getFilterChanger(todos, filter);
+    return (
+        <section>
+            <ul>
+                {
+                    filtered.map((item) => (
+                        <Todo key={item.id} todo={item} onUpdate={handleUpdate} onDelete={handleDelete} />
+                    ))
+                }
+            </ul>
+            <AddTodo onAdd={handleAdd}/>
+        </section>
+    );
+}
+
+function getFilterChanger(todos, filter) {
+    if(filter === 'all') {
+        return todos;
+    }
+    return todos.filter((todo) => todo.status === filter);
+}
+
