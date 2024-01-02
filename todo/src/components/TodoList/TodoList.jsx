188760d838fce2483c2cfcab427dfcaca8707a5c
Cheolmin99 <cjfals8570@naver.com>
2024-01-02 23:30:52 +0900

    스타일링 완료

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
+import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
     const [todos, setTodos] = useState([
@@ -12,8 +13,8 @@ export default function TodoList({ filter }) {
     const handleDelete = (deleted) => setTodos(todos.filter((todo) => (todo.id !== deleted.id)));
     const filtered = getFilterChanger(todos, filter);
     return (
-        <section>
-            <ul>
+        <section className={styles.container}>
+            <ul className={styles.list}>
                 {
                     filtered.map((item) => (
                         <Todo key={item.id} todo={item} onUpdate={handleUpdate} onDelete={handleDelete} />
