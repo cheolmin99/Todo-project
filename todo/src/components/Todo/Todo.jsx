ac8db6cc9806e3fec493b3b9081b0cb331e5c34a
Cheolmin99 <cjfals8570@naver.com>
2024-01-03 22:46:31 +0900

    로컬스토리지에 json형태로 저장하는 방법

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -3,15 +3,15 @@ import { FaTrashAlt } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
-    const { text, status } = todo;
+    const { id, text, status } = todo;
     const handleChange = (e) => {
         onUpdate({...todo, status: e.target.checked ? 'completed' : 'acitve'});
     }
     const handleDelete = () => onDelete(todo);
     return (
         <li className={styles.todo}>
-            <input className={styles.checkbox} type='checkbox' id='checkbox' checked={status === 'completed'} onChange={handleChange} />
-            <label htmlFor='checkbox' className={styles.text}>{text}</label>
+            <input className={styles.checkbox} type='checkbox' id={id} checked={status === 'completed'} onChange={handleChange} />
+            <label htmlFor={id} className={styles.text}>{text}</label>
             <span className={styles.icon}>
                 <button onClick={handleDelete} className={styles.button}><FaTrashAlt /></button>
             </span>
--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-    const [todos, setTodos] = useState([
-        {id: '1', text: '공부하기', status: 'active'},
-        {id: '2', text: '운동하기', status: 'active'}
-    ]);
+    const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
     const handleAdd = (todo) => setTodos([...todos, todo]);
     const handleUpdate = (updated) => setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
     const handleDelete = (deleted) => setTodos(todos.filter((todo) => (todo.id !== deleted.id)));
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    },[todos]);
+
     const filtered = getFilterChanger(todos, filter);
     return (
         <section className={styles.container}>
@@ -24,6 +26,11 @@ export default function TodoList({ filter }) {
             <AddTodo onAdd={handleAdd}/>
         </section>
     );
+}
+
+function readTodosFromLocalStorage() {
+    const todos = localStorage.getItem('todos');
+    return todos ? JSON.parse(todos) : [];
 }
 
 function getFilterChanger(todos, filter) {
